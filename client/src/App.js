5156b6105a9f417138553f717bd4472fc4d1815c
Bilal Azam <bilal.azam1@outlook.com>
2024-04-18 00:00:00 +0100

    Integrate TensorFlow.js for AI-based recommendations

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,4 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import LoginPage from './pages/LoginPage';
-import RegisterPage from './pages/RegisterPage';
+// Add the recommendations page route
+import RecommendationsPage from './pages/RecommendationsPage';
 
-function App() {
-    return (
-        <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/register" element={<RegisterPage />} />
-        </Routes>
-    );
-}
-
-export default App;
+<Route path="/recommendations" element={<RecommendationsPage />} />
