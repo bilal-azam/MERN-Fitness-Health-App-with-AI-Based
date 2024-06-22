baf89fe2f9fda4b6181aa793ad5e7dc004aac397
Bilal Azam <bilal.azam1@outlook.com>
2024-06-23 00:00:00 +0100

    Implement data caching to improve performance and reduce server load.

--- src/api/routes/externalApiRoutes.js
+++ src/api/routes/externalApiRoutes.js
@@ -1,8 +1,4 @@
-const express = require('express');
-const router = express.Router();
-const externalApiController = require('../controllers/externalApiController');
+const cacheMiddleware = require('../middleware/cacheMiddleware');
 
-router.get('/weather', externalApiController.getWeather);
-router.get('/news', externalApiController.getNews);
-
-module.exports = router;
+router.get('/weather', cacheMiddleware, externalApiController.getWeather);
+router.get('/news', cacheMiddleware, externalApiController.getNews);
