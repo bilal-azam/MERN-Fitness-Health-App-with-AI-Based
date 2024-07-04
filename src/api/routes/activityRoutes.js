b50505c49c6b071432c4134ba6ed2038e2862601
Bilal Azam <bilal.azam1@outlook.com>
2024-07-05 00:00:00 +0100

    Implement user activity logging.

--- src/api/routes/activityRoutes.js
+++ src/api/routes/activityRoutes.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const activityController = require('../controllers/activityController');
-const roles = require('../middleware/roles');
 
 router.post('/log', activityController.logActivity);
-router.get('/logs', roles('admin', 'user'), activityController.getActivityLogs);
+router.get('/', activityController.getActivities);
 
 module.exports = router;
