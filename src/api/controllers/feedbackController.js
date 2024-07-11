9ecc94e969c4a693c9a3f7adfd52a3a2159de23d
Bilal Azam <bilal.azam1@outlook.com>
2024-07-12 00:00:00 +0100

    Add user feedback and review system.

--- src/api/controllers/feedbackController.js
+++ src/api/controllers/feedbackController.js
@@ -2,13 +2,18 @@ const db = require('../models');
 
 exports.submitFeedback = async (req, res) => {
   try {
-      const { feedback, rating } = req.body;
-      await db.Feedback.create({
-          userId: req.user.id,
-          feedback,
-          rating
-      });
+      const { feedback } = req.body;
+      await db.Feedback.create({ userId: req.user.id, feedback });
       res.status(201).send('Feedback submitted');
+  } catch (error) {
+      res.status(500).send('Server error');
+  }
+};
+
+exports.getFeedbacks = async (req, res) => {
+  try {
+      const feedbacks = await db.Feedback.findAll();
+      res.json(feedbacks);
   } catch (error) {
       res.status(500).send('Server error');
   }
