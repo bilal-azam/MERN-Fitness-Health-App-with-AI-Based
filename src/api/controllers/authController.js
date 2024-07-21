9fda7f1a56b690dc95b7b880e39b2bac0f5c7170
Bilal Azam <bilal.azam1@outlook.com>
2024-07-22 00:00:00 +0100

    Add two-factor authentication (2FA) for user logins.

--- src/api/controllers/authController.js
+++ src/api/controllers/authController.js
@@ -1,12 +1,16 @@
-const speakeasy = require('speakeasy');
 const db = require('../models');
-const nodemailer = require('nodemailer');
+const speakeasy = require('speakeasy');
+const qrcode = require('qrcode');
 
-exports.generate2FASecret = async (req, res) => {
+exports.generate2FA = async (req, res) => {
   try {
-      const secret = speakeasy.generateSecret();
+      const secret = speakeasy.generateSecret({ name: 'MyApp' });
       await db.User.update({ twoFASecret: secret.base32 }, { where: { id: req.user.id } });
-      res.json({ secret: secret.base32 });
+      
+      qrcode.toDataURL(secret.otpauth_url, (err, data_url) => {
+          if (err) throw err;
+          res.json({ secret: secret.base32, qrCodeUrl: data_url });
+      });
   } catch (error) {
       res.status(500).send('Server error');
   }
@@ -21,8 +25,9 @@ exports.verify2FA = async (req, res) => {
           encoding: 'base32',
           token
       });
+
       if (verified) {
-          res.send('2FA verified');
+          res.send('2FA verification successful');
       } else {
           res.status(400).send('Invalid token');
       }
