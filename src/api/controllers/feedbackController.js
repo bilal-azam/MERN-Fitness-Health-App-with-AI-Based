14cfc71f4217deb9719f729db97f4f2e35183091
Bilal Azam <bilal.azam1@outlook.com>
2024-07-25 00:00:00 +0100

    Implement a feature for user feedback on system performance.

--- src/api/controllers/feedbackController.js
+++ src/api/controllers/feedbackController.js
@@ -3,17 +3,20 @@ const db = require('../models');
 exports.submitFeedback = async (req, res) => {
   try {
       const { feedback } = req.body;
-      await db.Feedback.create({ userId: req.user.id, feedback });
-      res.status(201).send('Feedback submitted');
+      await db.Feedback.create({
+          userId: req.user.id,
+          feedback
+      });
+      res.send('Feedback submitted');
   } catch (error) {
       res.status(500).send('Server error');
   }
 };
 
-exports.getFeedbacks = async (req, res) => {
+exports.getFeedback = async (req, res) => {
   try {
-      const feedbacks = await db.Feedback.findAll();
-      res.json(feedbacks);
+      const feedback = await db.Feedback.findAll({ where: { userId: req.user.id } });
+      res.json(feedback);
   } catch (error) {
       res.status(500).send('Server error');
   }
