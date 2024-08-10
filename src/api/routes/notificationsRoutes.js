99a0294de8228b3481d843cc2de61e0a8254379b
Bilal Azam <bilal.azam1@outlook.com>
2024-08-11 00:00:00 +0100

    Add feature for custom user notifications and alerts.

--- src/api/routes/notificationsRoutes.js
+++ src/api/routes/notificationsRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const notificationsController = require('../controllers/notificationsController');
 
+router.post('/create', notificationsController.createNotification);
 router.get('/', notificationsController.getNotifications);
 
 module.exports = router;
