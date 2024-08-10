18268f73e3bfb9c8d802705fdd62770a1a8272e0
Bilal Azam <bilal.azam1@outlook.com>
2024-08-11 00:00:00 +0100

    Add feature for custom user notifications and alerts.

--- src/api/controllers/notificationsController.js
+++ src/api/controllers/notificationsController.js
@@ -1,5 +1,19 @@
 const db = require('../models');
 
+exports.createNotification = async (req, res) => {
+  try {
+      const { message, type } = req.body;
+      await db.Notification.create({
+          userId: req.user.id,
+          message,
+          type
+      });
+      res.send('Notification created');
+  } catch (error) {
+      res.status(500).send('Server error');
+  }
+};
+
 exports.getNotifications = async (req, res) => {
   try {
       const notifications = await db.Notification.findAll({ where: { userId: req.user.id } });
