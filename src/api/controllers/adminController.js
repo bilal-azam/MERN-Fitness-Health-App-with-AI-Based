103078aa15550c5505eb142d691b44fef52bbdc2
Bilal Azam <bilal.azam1@outlook.com>
2024-08-12 00:00:00 +0100

    Implement an admin dashboard with user management functionalities.

--- src/api/controllers/adminController.js
+++ src/api/controllers/adminController.js
@@ -1,16 +1,19 @@
 const db = require('../models');
 
-exports.getSystemStats = async (req, res) => {
+exports.getUserList = async (req, res) => {
   try {
-      const userCount = await db.User.count();
-      const activityCount = await db.UserActivity.count();
-      const feedbackCount = await db.Feedback.count();
-      
-      res.json({
-          userCount,
-          activityCount,
-          feedbackCount
-      });
+      const users = await db.User.findAll();
+      res.json(users);
+  } catch (error) {
+      res.status(500).send('Server error');
+  }
+};
+
+exports.deleteUser = async (req, res) => {
+  try {
+      const { userId } = req.params;
+      await db.User.destroy({ where: { id: userId } });
+      res.send('User deleted');
   } catch (error) {
       res.status(500).send('Server error');
   }
