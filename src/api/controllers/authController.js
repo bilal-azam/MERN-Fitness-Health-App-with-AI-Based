f168f75d28ee0c075c0e177b92257ea89e9a68fa
Bilal Azam <bilal.azam1@outlook.com>
2024-08-19 00:00:00 +0100

    Add user authentication using JWT.

--- src/api/controllers/authController.js
+++ src/api/controllers/authController.js
@@ -1,37 +1,25 @@
+const jwt = require('jsonwebtoken');
 const db = require('../models');
-const speakeasy = require('speakeasy');
-const qrcode = require('qrcode');
 
-exports.generate2FA = async (req, res) => {
-  try {
-      const secret = speakeasy.generateSecret({ name: 'MyApp' });
-      await db.User.update({ twoFASecret: secret.base32 }, { where: { id: req.user.id } });
-      
-      qrcode.toDataURL(secret.otpauth_url, (err, data_url) => {
-          if (err) throw err;
-          res.json({ secret: secret.base32, qrCodeUrl: data_url });
-      });
-  } catch (error) {
-      res.status(500).send('Server error');
+exports.login = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await db.User.findOne({ where: { email } });
+
+  if (user && user.password === password) {
+      const token = jwt.sign({ id: user.id }, 'your_jwt_secret', { expiresIn: '1h' });
+      res.json({ token });
+  } else {
+      res.status(401).send('Invalid credentials');
   }
 };
 
-exports.verify2FA = async (req, res) => {
-  try {
-      const { token } = req.body;
-      const user = await db.User.findByPk(req.user.id);
-      const verified = speakeasy.totp.verify({
-          secret: user.twoFASecret,
-          encoding: 'base32',
-          token
-      });
+exports.verifyToken = (req, res, next) => {
+  const token = req.headers['x-auth-token'];
+  if (!token) return res.status(403).send('No token provided');
 
-      if (verified) {
-          res.send('2FA verification successful');
-      } else {
-          res.status(400).send('Invalid token');
-      }
-  } catch (error) {
-      res.status(500).send('Server error');
-  }
+  jwt.verify(token, 'your_jwt_secret', (err, decoded) => {
+      if (err) return res.status(500).send('Failed to authenticate token');
+      req.userId = decoded.id;
+      next();
+  });
 };
